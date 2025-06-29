4b9a2a9efcecbbba4017c018db4ef77d67ea876a
Himaanshuu-dotcom <himanshudetwani03@gmail.com>
2025-06-29 06:19:10 +0000

    datatypeeeee

---
+++ datatypes.js
@@ -0,0 +1,13 @@
+// PRIMITIVE
+// 7 types: string,number,boolean,null,undefined,symbol,BigInt
+
+
+// REFERENCE(NON PRIMITIVE)
+// Array,objects,functions
+const heroes=["himansu","harh"];
+// object example:-
+
+let myobj={
+    name:"himanshu",
+    age:22,
+}
